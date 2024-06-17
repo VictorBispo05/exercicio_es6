6f9f52d6e0cf51bcc491776b2a154910950b5f47
Victor Bispo <VictorBispo05@outlook.com>
2024-06-16 21:37:58 -0300

    correção

--- main.js
+++ main.js
@@ -1,39 +1,12 @@
-class Aluno {
-    nome = '';
-    nota = '';
+const alunos = [
+    { nome: 'Victor', nota: 7 },
+    { nome: 'Miguel', nota: 5 },
+    { nome: 'Leticia', nota: 10 }
+];
 
-    constructor(nomeDoAluno, notaDoAluno) {
-        this.nome = nomeDoAluno;
-        this.nota = notaDoAluno;
-    }
+function maiorQueSeis(alunos) {
+    return alunos.filter(aluno => aluno.nota >= 6);
 }
 
-const Victor = new Aluno();
-Victor.nome = 'Victor';
-Victor.nota = 6;
-
-const Leticia = new Aluno();
-Leticia.nome = 'Leticia';
-Leticia.nota = 10;
-
-const Miguel = new Aluno();
-Miguel.nome = 'Miguel';
-Miguel.nota = 4;
-
-const Alunos = [Victor, Leticia, Miguel]
-
-function notaMaiorQueSeis(Alunos) {
-    var notaNovaVictor = parseInt(Victor.nota)
-    if (notaNovaVictor>=6) console.log(Alunos[0]) 
-    else console.log(Victor.nome, "não tem nota maior que seis")
-
-    var notaNovaLeticia = parseInt(Leticia.nota)
-    if (notaNovaLeticia>=6) console.log(Alunos[1]) 
-    else console.log(Leticia.nome, "não tem nota maior que seis")
-
-    var notaNovaMiguel = parseInt(Miguel.nota)
-    if (notaNovaMiguel>=6) console.log(Alunos[2]) 
-    else console.log(Miguel.nome, "não tem nota maior que seis")
-}
-
-notaMaiorQueSeis(Alunos);
+const alunosAprovados = maiorQueSeis(alunos);
+console.log(alunosAprovados);
